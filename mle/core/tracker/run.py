3cf0f56bda31876ba9290e85c8bea894020f7309
xames3 <xames3.developer@gmail.com>
2020-01-14 14:39:11 +0530

    Refactor, update docstring & add internal comments
    * Refactor code to stick to Google's style guide approach of using 2
      spaces instead of 4.
    * Update docstring to make it more descriptive and intuitive.
    * Follow best practices of python (simple imports).
    * Add internal comments to quickly describe the need for it.
    * Fix general typos.

--- mle/core/tracker/run.py
+++ mle/core/tracker/run.py
@@ -14,22 +14,22 @@
 # limitations under the License.
 #
 # ======================================================================
-"""The ``mle.core.tracker.run`` module.
+"""The `mle.core.tracker.run` module.
 
-This module initiates MLE's tracking service in the background as soon
-as the system boots up. The service tracks the time spent on all valid
-active screens/applications and generates a csv file on daily basis.
+The core inspiration behind this module is to record the time spent on
+each application by the user. This data aims to provide 3 key insights:
+	1. How long the user uses a particular application or system?
+	2. Recommendations for using applications.
+	3. Overusage warning of a particular application.
 
-The generated reports are stored under ``./mle/data/tracker/`` directory
+The module is configured to run at windows boot and keep it running in
+background tracking the time spent on each application. And not only
+that but, record the usage of each application in a csv file on a daily
+basis.
+
+The generated reports are stored under `./mle/data/tracker/` directory
 with their respective dates.
-
-Todo:
-    * Remove ``pylint`` warning comments.
-
 """
-# The following comment should be removed at some point in the future.
-# pylint: disable=import-error
-# pylint: disable=no-name-in-module
 
 import os
 import time
@@ -38,53 +38,51 @@ from datetime import datetime, timedelta
 from win10toast import ToastNotifier
 
 from mle.core.tracker.track import get_active_window, split_time_spent
-from mle.core.tracker.write import update_data
 from mle.utils.common import mle_path
-from mle.vars.dev import DAY_LIMIT, DAY_LIMIT_FORMAT, CSV_TS_FORMAT
+from mle.utils.write import update_data
+from mle.vars import dev
 
 toast = ToastNotifier()
+header = ['activity', 'app', 'executable', 'user', 'started',
+          'stopped', 'spent', 'days', 'hours', 'mins', 'secs']
+previous_window = previous_app = previous_exe = previous_user = ''
+data_path = os.path.join(mle_path, 'data\\tracker')
+csv = ''.join([datetime.now().strftime(dev.CSV_TS_FORMAT), '.csv'])
+file = os.path.join(data_path, csv)
 
 try:
-    toast.show_toast(title='MLE Activity Tracker',
-                     msg='Tracker service started.')
-
-    previous_window = previous_app = previous_exe = previous_user = ''
-
-    start_time = datetime.now().replace(microsecond=0)
-
-    data_path = os.path.join(mle_path, 'data\\tracker')
-    csv = ''.join([datetime.now().strftime(CSV_TS_FORMAT), '.csv'])
-    file = os.path.join(data_path, csv)
-
-    while True:
-        active_window, active_app, active_exe, active_user = get_active_window()
-
-        if datetime.now().strftime(DAY_LIMIT_FORMAT) >= DAY_LIMIT:
-            new_date = datetime.now() + timedelta(days=1)
-            csv = ''.join([new_date.strftime(CSV_TS_FORMAT), '.csv'])
-            file = os.path.join(data_path, csv)
-
-        if active_window and active_window != 'Task Switching':
-            if (previous_window != active_window
-                and datetime.now().strftime(DAY_LIMIT_FORMAT) != DAY_LIMIT):
-                end_time = datetime.now().replace(microsecond=0)
-                total_time = end_time - start_time
-                spent_secs = total_time.total_seconds()
-                usage = split_time_spent(total_time)
-
-                if usage != (0, 0, 0, 0):
-                    update_data(file, previous_window, previous_app,
-                                previous_exe, previous_user, start_time, end_time, spent_secs, usage[0], usage[1],
-                                usage[2], usage[3])
-                    start_time = datetime.now().replace(microsecond=0)
-
-            previous_window = active_window
-            previous_app = active_app
-            previous_exe = active_exe
-            previous_user = active_user
-
-        time.sleep(1)
+	toast.show_toast(title='MLE Activity Tracker', msg='Tracker service started.')
+	start_time = datetime.now().replace(microsecond=0)
+	# Keep the service running.
+	while True:
+		active_window, active_app, active_exe, active_user = get_active_window()
+		# If the current time exceeds beyond the DAY_LIMIT save new records
+		# to new file.
+		if datetime.now().strftime(dev.DAY_LIMIT_FORMAT) >= dev.DAY_LIMIT:
+			new_date = datetime.now() + timedelta(days=1)
+			csv = ''.join([new_date.strftime(dev.CSV_TS_FORMAT), '.csv'])
+			file = os.path.join(data_path, csv)
+		# Skip Task Switching application.
+		if active_window and active_window != 'Task Switching':
+			if (previous_window != active_window
+					and datetime.now().strftime(dev.DAY_LIMIT_FORMAT) != dev.DAY_LIMIT):
+				end_time = datetime.now().replace(microsecond=0)
+				total_time = end_time - start_time
+				spent_secs = total_time.total_seconds()
+				usage = split_time_spent(total_time)
+				# Only track applications which are used for more than 1 second.
+				if usage != (0, 0, 0, 0):
+					update_data(file, header, previous_window, previous_app, 
+											previous_exe, previous_user, start_time, end_time,
+											spent_secs, usage[0], usage[1], usage[2], usage[3])
+					start_time = datetime.now().replace(microsecond=0)
+			previous_window = active_window
+			previous_app = active_app
+			previous_exe = active_exe
+			previous_user = active_user
+		# Check if the application is changed after 1 second.
+		time.sleep(1)
 except Exception:
-    toast.show_toast(title="MLE Activity Tracker - Error notification",
-                     msg='Tracker service failure.')
-    raise Exception('Tracker service failure.')
+	toast.show_toast(title='MLE Activity Tracker - Error notification',
+									 msg='Tracker service failure.')
+	raise Exception('Tracker service failure.')
