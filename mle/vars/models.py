d5020a3bd4e2895ecd2b808249ea72bb7dddbb8d
xames3 <xames3.developer@gmail.com>
2020-02-09 21:23:42 +0530

    Rename text detecting confidence & east model obj

--- mle/vars/models.py
+++ mle/vars/models.py
@@ -19,8 +19,8 @@
 # OpenCV models path
 FACE_PROTOTEXT = 'deploy.prototxt.txt'
 FACE_CAFFEMODEL = 'res10_300x300_ssd_iter_140000.caffemodel'
-TEXT_DETECTOR_EAST = 'frozen_east_text_detection.pb'
+TEXT_EAST_DETECTOR = 'frozen_east_text_detection.pb'
 
 # Confidence scores
 DETECTED_FACE_CONFIDENCE = 0.7
-DETECTED_TEXT_MIN_CONFIDENCE = 0.6
+DETECTED_TEXT_CONFIDENCE = 0.6
