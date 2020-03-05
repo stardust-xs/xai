7f91805d19a8c3e751b827688480afa4da2c652a
xames3 <xames3.developer@gmail.com>
2020-03-05 23:02:46 +0530

    Add draw_label_box() and more refactoring
    * Add draw_label_box() to display the label of the detection relative
      to the position of the bounding box.
    * Add support for draw_label_box() in draw_bounding_box() with updated
      docstrings.
    * Remove draw_centroid() as it didn't serve any purpose.
    * Clean up unnecessary imports.

--- mle/utils/opencv.py
+++ mle/utils/opencv.py
@@ -16,13 +16,12 @@
 # ======================================================================
 """Utility for making convenient use of OpenCV."""
 
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Tuple, Union
 
 # TODO(xames3): Remove suppressed pyright warnings.
 # pyright: reportMissingTypeStubs=false
 import cv2
 import numpy as np
-import imutils
 
 from mle.constants import colors
 
@@ -71,11 +70,75 @@ def disconnect(stream: np.ndarray) -> None:
   cv2.destroyAllWindows()
 
 
+def draw_label_box(frame: np.ndarray,
+                   overlay: np.ndarray,
+                   x0_y0: Tuple,
+                   x1_y1: Tuple,
+                   label: str,
+                   confidence: Union[float, int],
+                   label_color: Tuple = colors.WHITE,
+                   color: Tuple = colors.BLACK,
+                   alpha: Union[float, int] = 0.3,
+                   alpha_color: Tuple = colors.BLACK,
+                   thickness: int = 1) -> None:
+  """Draw box for labels relative to the bounding box.
+
+  Draws a label box relative to the bounding box. This label box display
+  label/name & the confidence score of the detected object.
+
+  Args:
+    frame: Numpy array of the image frame.
+    overlay: Copy of the image frame.
+    x0_y0: Tuple of top left coordinates.
+    x1_y1: Tuple of bottom right coordinates.
+    label: Label/Name of the detected object.
+    confidence: Confidence score of the detection.
+    label_color: Label color (default: white).
+    color: Bounding box (default: black) color.
+    alpha: Opacity of the detected region overlay.
+    alpha_color: Overlayed color (default: black).
+    thickness: Thickness (default: 1) of the bounding box.
+  """
+  # Unpacking tuples into 4 seperate points.
+  (x0, y0), (_, y1) = x0_y0, x1_y1
+  # Position of the label box by default. Start from the X - coordinate,
+  # just above the bounding box.
+  x3, y3 = x0, (y0 - 5)
+  # Default label - confidence template.
+  label = f'{label} | {confidence}%'
+  # If bounding box is at the left edge of the view, display the label
+  # box at the left edge.
+  if x3 < 0:
+    x3 = 0
+  # If bounding box is at the right edge of the view, display the label
+  # box at the right edge by considering the label as well.
+  elif (x3 + 10 + (len(label) * 7) > frame.shape[1]):
+    x3 = x3 - (x3 + 10 + len(label) * 7 - (frame.shape[1])) - 10
+  # If the bounding box is high up towards the top, display the label
+  # box at the bottom of bounding box.
+  if y3 < 0:
+    y3 = y1 + 30
+  # Initializing new coordinates with adjustments.
+  # NOTE: These adjustments are subjective and may vary in future.
+  x4, y4 = int(x3), int(y3 - 25)
+  x5, y5 = int(x3 + 20 + (len(label) * 7)), int(y3 - 1)
+  # Adding an alpha bounding box, similar to draw_bounding_box().
+  cv2.rectangle(overlay, (x4, y4), (x5, y5), alpha_color, -1)
+  cv2.rectangle(frame, (x4, y4), (x5, y5), color, thickness)
+  cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+  # Adding label & confidence score.
+  cv2.putText(frame, label, (int(x3 + 10), int(y3 - 10)),
+              cv2.FONT_HERSHEY_DUPLEX, 0.4, label_color)
+
+
 def draw_bounding_box(frame: np.ndarray,
                       x0_y0: Tuple,
                       x1_y1: Tuple,
+                      label: str,
+                      confidence: Union[float, int],
+                      label_color: Tuple = colors.WHITE,
                       color: Tuple = colors.RED,
-                      alpha: Union[float, int] = 0.3,
+                      alpha: Union[float, int] = 0.2,
                       alpha_color: Tuple = colors.RED,
                       thickness: int = 2) -> None:
   """Draw bounding box using the Numpy tuple.
@@ -87,15 +150,17 @@ def draw_bounding_box(frame: np.ndarray,
     frame: Numpy array of the image frame.
     x0_y0: Tuple of top left coordinates.
     x1_y1: Tuple of bottom right coordinates.
+    label: Label/Name of the detected object.
+    confidence: Confidence score of the detection.
+    label_color: Label color (default: white).
     color: Bounding box (default: red) color.
     alpha: Opacity of the detected region overlay.
     alpha_color: Overlayed color (default: red).
-    thickness: Thickness (default: 1) of the bounding box.
+    thickness: Thickness (default: 2) of the bounding box.
 
   Note:
     This method can be used for drawing the bounding boxes around
-    objects whose coordinates are derived from a Machine Learning based
-    model.
+    objects whose coordinates are derived from a ML based models.
 
   Usage:
     * For Haar based detections, use the below settings -
@@ -104,40 +169,12 @@ def draw_bounding_box(frame: np.ndarray,
         (x0, y0), (x1, y1) = x0_y0, x1_y1
         cv2.rectangle(frame, (x0, y1), (x1, y1 + 20), color, -1)
   """
-  overlay = frame.copy()
   # Transparent/Alpha overlay rectangle layer.
+  overlay = frame.copy()
   cv2.rectangle(overlay, x0_y0, x1_y1, alpha_color, -1)
   # Main bounding box.
   cv2.rectangle(frame, x0_y0, x1_y1, color, thickness)
   cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
-
-
-def draw_centroid(frame: np.ndarray,
-                  radius: int = 5,
-                  color: Tuple = colors.WHITE,
-                  thickness: int = 1) -> None:
-  """Draw centroid for the detected shape/contour.
-
-  Draw a centroid for the detected shape/contour or an sharp edge.
-
-  Args:
-    frame: Numpy array of the image frame.
-    radius: Radius of the circle representing the centroid.
-    color: Bounding box (default: white) color.
-    thickness: Thickness (default: 1) of the bounding box.
-  """
-  # TODO(xames3): Add more details regarding below code lines.
-  # Convert the colored image into grayscale.
-  gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  blur_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-  threshold = cv2.threshold(blur_frame, 60, 255, cv2.THRESH_BINARY)[1]
-  contours = cv2.findContours(threshold.copy(),
-                              cv2.RETR_EXTERNAL,
-                              cv2.CHAIN_APPROX_SIMPLE)
-  contours = imutils.grab_contours(contours)
-  for contour in contours:
-    moment = cv2.moments(contour)
-    x = int(moment['m10'] / moment['m00']) if moment['m00'] > 0 else 0
-    y = int(moment['m01'] / moment['m00']) if moment['m00'] > 0 else 0
-    cv2.drawContours(frame, [contour], -1, color, thickness)
-    cv2.circle(frame, (x, y), radius, color, -1)
+  # Draw label box relative to the bounding box.
+  draw_label_box(frame, overlay, x0_y0, x1_y1, label,
+                 confidence, label_color, colors.BLACK)
