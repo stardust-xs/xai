b8c804a722a09874129e1e0a2e23b7baf705a6c3
xames3 <xames3.developer@gmail.com>
2020-03-24 02:46:10 +0530

    Add log() to track errors/info/warnings via logs

--- mle/utils/common.py
+++ mle/utils/common.py
@@ -18,16 +18,21 @@
 
 import cProfile
 import io
+import logging
+import os
 import pstats
 import random
 import socket
+import sys
 from datetime import datetime
+from pathlib import Path
 from typing import Callable, List, Optional, Tuple, Union
 
 from fuzzywuzzy import fuzz, process
 from win10toast import ToastNotifier
 
 from mle.constants import colors, defaults
+from mle.utils import symlinks
 
 
 def find_string(string: str,
@@ -125,3 +130,32 @@ def pick_random_color() -> Tuple:
 def timestamp(timestamp_format: str = '%d_%m_Y_%H_%M_%S') -> str:
   """Returns string with current timestamp with a specified format."""
   return now().strftime(timestamp_format)
+
+
+def log(file: str, level: str = 'debug') -> logging.Logger:
+  """Create log file and log print events.
+
+  Args:
+    file: Current file name.
+    level: Logging level.
+
+  Returns:
+    Logger object which records logs in ./logs/ directory.
+  """
+  logger = logging.getLogger(file)
+  logger.setLevel(f'{level.upper()}')
+  name = f'{Path(file.lower()).stem}.log'
+  name = Path(os.path.join(symlinks.logs, name))
+  formatter = logging.Formatter(f'%(asctime)s.%(msecs)05d    %(levelname)'
+                                '-8s    %(filename)s:%(lineno)-16s   %(message)'
+                                '-8s', '%Y-%m-%d %H:%M:%S')
+  # Create log file.
+  file_handler = logging.FileHandler(os.path.join(symlinks.logs, name))
+  file_handler.setFormatter(formatter)
+  logger.addHandler(file_handler)
+  # Print log statement.
+  stream_handler = logging.StreamHandler(sys.stdout)
+  stream_handler.setFormatter(formatter)
+  logger.addHandler(stream_handler)
+  # Return logger object.
+  return logger
