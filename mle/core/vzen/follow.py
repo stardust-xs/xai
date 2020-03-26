ec67f3805a1a5a0c6f1d6a4f061f526d9a937ea2
xames3 <xames3.developer@gmail.com>
2020-03-26 12:04:06 +0530

    Rename ./vzen/run.py to ./vzen/follow.py

--- mle/core/vzen/follow.py
+++ mle/core/vzen/follow.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 # ======================================================================
-"""The `mle.core.vzen.run` module.
+"""The `mle.core.vzen.follow` module.
 
 The core inspiration behind this module is to use computer vision to
 it's full potential and to understand more about image processing and
@@ -28,31 +28,47 @@ running in background or provide some kind of service or let alone run
 independently.
 
 Any/All generated reports are stored in `./mle/data/raw/vzen/` with
-their respective names.
+their respective names & logs under `./mle/logs/follow.log`.
 """
 
 import time
 
 import cv2
 
+from mle.constants import defaults as dx
 from mle.core.vzen.subservices.face_detector import detect_faces
-from mle.utils.common import vzen_toast
+from mle.utils.common import log
+from mle.utils.common import toast as toast
 from mle.utils.opencv import disconnect, rescale
 
+log = log(__file__)
+
 while True:
-  stream = cv2.VideoCapture(0)
-  vzen_toast('MLE VZen', 'VZen service started.')
-  # time.sleep(2.0)
-  # Keep the service running.
-  while stream.isOpened():
-    _, frame = stream.read()
-    frame = rescale(frame, width=400)
-    detect_faces(frame)
-    # Terminate the stream after pressing 'Esc' key.
-    cv2.imshow('MLE VZen', frame)
-    if cv2.waitKey(1) & 0xFF == int(27):
-      disconnect(stream)
-      exit(0)
-  else:
-    vzen_toast('MLE VZen - Warning Notification', 'VZen service broken.')
-    time.sleep(15.0)
+  try:
+    stream = cv2.VideoCapture(0)
+    log.info('Vzen service started.')
+    toast('MLE VZen', 'VZen service started.')
+    time.sleep(2.0)
+    # Keep the service running.
+    while stream.isOpened():
+      _, frame = stream.read()
+      frame = rescale(frame, width=400)
+      detect_faces(frame)
+      # Terminate the stream after pressing 'Esc' key.
+      cv2.imshow('MLE VZen', frame)
+      if cv2.waitKey(1) & 0xFF == int(27):
+        disconnect(stream)
+        exit(0)
+    else:
+      log.warning('VZen service broken.')
+      toast('MLE VZen - Warning Notification', 'VZen service broken.')
+      time.sleep(dx.EXCEPTION_TIMER)
+  except KeyboardInterrupt:
+    log.error('VZen service interrupted.')
+    toast('MLE VZen - Error Notification', 'VZen service interrupted.')
+  except Exception:
+    log.critical('Something went wrong with vzen service.')
+    toast('MLE VZen - Error Notification',
+          'Something went wrong with the service.')
+    log.warning('Restarting VZen service.')
+    time.sleep(dx.EXCEPTION_TIMER)
