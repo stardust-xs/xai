45ce06703592f33b32ed4cef52fc2c9c3691e2fd
xames3 <xames3.developer@gmail.com>
2020-04-08 22:42:52 +0530

    Refactor, add face orientation & improve speed
    * MLE VZen view is not rescaled now.
    * Add support for face orientation.
    * 100% performance boost from avg. 7-9 fps to 15-18 fps. since
      all models are loaded just once.
    * Add support for calculating FPS & Elapsed time in the view.
    * Add support to disconnect the stream if something goes wrong.
    * Add support to terminate the service if interrupted by a keyboard
      interrupt.
    * The camera sleeps for 1 sec. instead of 2 secs.

--- mle/core/vzen/follow.py
+++ mle/core/vzen/follow.py
@@ -35,30 +35,48 @@ import time
 
 import cv2
 
+from dlib import shape_predictor
 from mle.constants import defaults as dx
 from mle.core.vzen.subservices.face_detector import detect_faces
-from mle.utils.common import log
-from mle.utils.common import toast as toast
-from mle.utils.opencv import disconnect, rescale
+from mle.utils import symlinks
+from mle.utils.common import log, now, seconds_to_datetime
+from mle.utils.common import vzen_toast as toast
+from mle.utils.opencv import disconnect, draw_statistics_box
 
 log = log(__file__)
+face_net = cv2.dnn.readNetFromCaffe(symlinks.prototext, symlinks.caffemodel)
+face_predictor = shape_predictor(symlinks.face_landmarks_68_pts)
 
 while True:
+  stream = cv2.VideoCapture(0)
+  log.info('Vzen service started.')
+  toast('MLE VZen', 'VZen service started.')
+  time.sleep(dx.REFRESH_TIMER)
+  frame_number = 0
+  start = now()
   try:
-    stream = cv2.VideoCapture(0)
-    log.info('Vzen service started.')
-    toast('MLE VZen', 'VZen service started.')
-    time.sleep(2.0)
     # Keep the service running.
     while stream.isOpened():
       _, frame = stream.read()
-      frame = rescale(frame, width=400)
-      detect_faces(frame)
+      # You can learn more about blob here:
+      # https://www.pyimagesearch.com/2017/11/06/deep-learning-opencvs-blobfromimage-works/
+      face_net.setInput(cv2.dnn.blobFromImage(frame, 1.0, (299, 299),
+                                              (104.0, 177.0, 123.0)))
+      detect_faces(frame, face_net.forward(), face_predictor)
+      # Calculate and display statistics.
+      if frame_number > dx.BUFFER_FRAME_THRESHOLD:
+        height, width, _ = frame.shape
+        fps = round((frame_number / (now() - start).seconds), 2)
+        elapsed_time = seconds_to_datetime((now() - start).seconds)
+        statistics = f'FPS : {fps}\nELAPSED : {elapsed_time}'
+        draw_statistics_box(frame, statistics, (5, height))
       # Terminate the stream after pressing 'Esc' key.
       cv2.imshow('MLE VZen', frame)
       if cv2.waitKey(1) & 0xFF == int(27):
         disconnect(stream)
+        log.warning('VZen service terminated.')
         exit(0)
+      frame_number += 1
     else:
       log.warning('VZen service broken.')
       toast('MLE VZen - Warning Notification', 'VZen service broken.')
@@ -66,9 +84,11 @@ while True:
   except KeyboardInterrupt:
     log.error('VZen service interrupted.')
     toast('MLE VZen - Error Notification', 'VZen service interrupted.')
+    exit(0)
   except Exception:
     log.critical('Something went wrong with vzen service.')
     toast('MLE VZen - Error Notification',
           'Something went wrong with the service.')
+    disconnect(stream)
     log.warning('Restarting VZen service.')
     time.sleep(dx.EXCEPTION_TIMER)
