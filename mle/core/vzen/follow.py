7a137b283e3a0109b320026315a3633ca8fccf2b
xames3 <xames3.developer@gmail.com>
2020-04-15 22:08:26 +0530

    Update follow module to support YOLOv3 model

--- mle/core/vzen/follow.py
+++ mle/core/vzen/follow.py
@@ -35,20 +35,25 @@ import time
 
 import cv2
 
-from dlib import shape_predictor
 from mle.constants import defaults as dx
-from mle.core.vzen.subservices.face_detector import detect_faces
-from mle.utils import symlinks
+from mle.core.vzen.subservices.detector import detect_faces_using_yolo
 from mle.utils.common import log, now, seconds_to_datetime
 from mle.utils.common import vzen_toast as toast
-from mle.utils.opencv import disconnect, draw_statistics_box
+from mle.utils.opencv import disconnect, draw_statistics_box, rescale
+from mle.utils.symlinks import yolov3_config, yolov3_weights
 
+# Logging MLE's follow module's activities.
 log = log(__file__)
-face_net = cv2.dnn.readNetFromCaffe(symlinks.prototext, symlinks.caffemodel)
-face_predictor = shape_predictor(symlinks.face_landmarks_68_pts)
+# Using YOLOv3 algorithm for making detections.
+net = cv2.dnn.readNetFromDarknet(yolov3_config, yolov3_weights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+layer_names = net.getLayerNames()
+output_layers = [layer_names[idx[0] - 1]
+                 for idx in net.getUnconnectedOutLayers()]
 
 while True:
-  stream = cv2.VideoCapture(0)
+  stream = cv2.VideoCapture(r'D:\multimedia\videos\test_videos\skype.mp4')
   log.info('Vzen service started.')
   toast('MLE VZen', 'VZen service started.')
   time.sleep(dx.REFRESH_TIMER)
@@ -58,18 +63,19 @@ while True:
     # Keep the service running.
     while stream.isOpened():
       _, frame = stream.read()
+      # frame = rescale(frame, 400)
       # You can learn more about blob here:
       # https://www.pyimagesearch.com/2017/11/06/deep-learning-opencvs-blobfromimage-works/
-      face_net.setInput(cv2.dnn.blobFromImage(frame, 1.0, (299, 299),
-                                              (104.0, 177.0, 123.0)))
-      detect_faces(frame, face_net.forward(), face_predictor)
+      net.setInput(cv2.dnn.blobFromImage(frame, 0.00392, (416, 416),
+                                         (0, 0, 0), True, crop=False))
+      detect_faces_using_yolo(frame, net.forward(output_layers))
       # Calculate and display statistics.
-      if frame_number > dx.BUFFER_FRAME_THRESHOLD:
+      if frame_number > dx.BUFFER_FRAMES:
         height, width, _ = frame.shape
         fps = round((frame_number / (now() - start).seconds), 2)
         elapsed_time = seconds_to_datetime((now() - start).seconds)
         statistics = f'FPS : {fps}\nELAPSED : {elapsed_time}'
-        draw_statistics_box(frame, statistics, (5, height))
+        draw_statistics_box(frame, (5, height), statistics)
       # Terminate the stream after pressing 'Esc' key.
       cv2.imshow('MLE VZen', frame)
       if cv2.waitKey(1) & 0xFF == int(27):
