b7d5b168229573d11cfbab5dda3f0d9d6f65777b
xames3 <xames3.developer@gmail.com>
2020-04-18 01:28:13 +0530

    Remove periods from inline comments Saturday, April 18 2020 - v2.6

--- mle/utils/symlinks.py
+++ mle/utils/symlinks.py
@@ -20,10 +20,10 @@ import os
 
 from mle.constants import models as _models
 
-# Parent directory symlink.
+# Parent directory symlink
 parent = os.path.dirname(os.path.dirname(__file__))
 
-# Subsequent symlinks.
+# Subsequent symlinks
 data = os.path.join(parent, 'data')
 files = os.path.join(parent, 'files')
 logs = os.path.join(parent, 'logs')
@@ -31,21 +31,21 @@ models = os.path.join(parent, 'models')
 resources = os.path.join(parent, 'resources')
 stats = os.path.join(parent, 'stats')
 
-# Child symlinks.
+# Child symlinks
 raw = os.path.join(data, 'raw')
 test = os.path.join(data, 'test')
 train = os.path.join(data, 'train')
 validation = os.path.join(data, 'validation')
 
-# Grandchild symlinks.
+# Grandchild symlinks
 monitor = os.path.join(raw, 'monitor')
 vzen = os.path.join(raw, 'vzen')
 
-# Symlinks to save all data for training purposes.
+# Symlinks to save all data for training purposes
 usage = os.path.join(monitor, 'usage')
 weather = os.path.join(monitor, 'weather')
 
-# Symlinks to all models related to OpenCV.
+# Symlinks to all models related to OpenCV
 caffemodel = os.path.join(models, _models.CAFFEMODEL)
 prototext = os.path.join(models, _models.PROTOTEXT)
 landmarks_5_pts = os.path.join(models, _models.LANDMARKS_5_POINTS)
@@ -54,5 +54,5 @@ east_text_detector = os.path.join(models, _models.EAST_TEXT_DETECTOR)
 yolov3_config = os.path.join(models, _models.YOLO_V3_CONFIG)
 yolov3_weights = os.path.join(models, _models.YOLO_V3_WEIGHTS)
 
-# Symlinks to all resources.
+# Symlinks to all resources
 arame = os.path.join(resources, 'arame.ttf')
