83e36328d0cc5f34f0397e9ec96fb117f3b01d46
xames3 <xames3.developer@gmail.com>
2020-05-08 21:59:18 +0530

    Add version support

---
+++ xai/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2020 XAMES3. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ======================================================================
+
+# This version string is semver compatible & adheres to Semantic
+# Versioning Specification (SemVer) starting with version 0.1.
+# You can read more about it here: https://semver.org/spec/v2.0.0.html
+__version__ = '3.0.0'
+
+# Version history
+
+# 3.0.1 - May 04, 2020
+# 3.0.0 - Apr 29, 2020
+# 2.0.1 - Feb 29, 2020
+# 0.0.1 - Jan 01, 2020
