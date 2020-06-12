b3773ef88fa447d7fef3d1bc7a6b13ce90b5af59
xames3 <xames3.developer@gmail.com>
2020-06-12 22:47:14 +0530

    Add support for CPU usage along with RAM usage

--- xai/core/vzen.py
+++ xai/core/vzen.py
@@ -217,8 +217,9 @@ class GodsEye(object):
           frm = cv2.resize(frm, None, fx=self._scl, fy=self._scl,
                            interpolation=cv2.INTER_AREA)
 
-          ram = f'RAM : {resolve_size(psutil.virtual_memory().used)}'
-          smart_text_box(frm, 5, frm.shape[0] - 30, 0, 0, ram)
+          res = (f'RAM : {resolve_size(psutil.virtual_memory().used)} '
+                 f'CPU : {psutil.Process(self._pid).cpu_percent(interval=0)}%')
+          smart_text_box(frm, 5, frm.shape[0] - 30, 0, 0, res)
 
           # Records the time the session has started. This lets X.AI to
           # calculate the FPS at which the camera(s) are recording.
